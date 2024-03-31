a11350d7713972bad6f0a4461e35458e376c1c2a
aniketgarje7 <garjeaniket7@gmail.com>
2024-04-01 00:22:42 +0530

    changes

--- src/Components/Products.tsx
+++ src/Components/Products.tsx
@@ -1,7 +1,5 @@
 import ProductCard, { Product } from "./Card";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 type ProductsProp = {
   products?: Product[];
